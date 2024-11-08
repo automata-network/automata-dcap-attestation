890aaa3d4990ef2f608d8a67d27002a75a3f6cb0
Preston Ong <preston4896@me.com>
2024-11-08 18:37:13 +0800

    commenting out sp1 groth16 test code because its failing ci due to compiler config

--- forge-test/utils/SuccinctSetup.sol
+++ forge-test/utils/SuccinctSetup.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {SP1Verifier} from "@sp1-contracts/v3.0.0/SP1VerifierGroth16.sol";
-// import {SP1Verifier} from "@sp1-contracts/v3.0.0/SP1VerifierPlonk.sol";
+// import {SP1Verifier} from "@sp1-contracts/v3.0.0/SP1VerifierGroth16.sol";
+import {SP1Verifier} from "@sp1-contracts/v3.0.0/SP1VerifierPlonk.sol";
 import {ISP1Verifier} from "@sp1-contracts/ISP1Verifier.sol";
 
 enum ProveOpt {
@@ -12,14 +12,14 @@ enum ProveOpt {
 
 abstract contract SuccinctSetup {
     function setupSP1Verifier(ProveOpt opt) internal returns (ISP1Verifier verifier) {
-        if (opt == ProveOpt.Groth16) {
-            SP1Verifier groth16Verifier = new SP1Verifier();
-            verifier = ISP1Verifier(address(groth16Verifier));
-        }
-        // } else
-        // if (opt == ProveOpt.Plonk) {
-        //     SP1Verifier plonkVerifier = new SP1Verifier();
-        //     verifier = ISP1Verifier(address(plonkVerifier));
+        // if (opt == ProveOpt.Groth16) {
+        //     SP1Verifier groth16Verifier = new SP1Verifier();
+        //     verifier = ISP1Verifier(address(groth16Verifier));
         // }
+        // } else
+        if (opt == ProveOpt.Plonk) {
+            SP1Verifier plonkVerifier = new SP1Verifier();
+            verifier = ISP1Verifier(address(plonkVerifier));
+        }
     }
 }
