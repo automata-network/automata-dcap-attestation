57739135a550b520d83232b78aab741855404cdf
Preston Ong <preston4896@me.com>
2024-11-19 14:42:25 +0800

    risczero passes unit test

--- forge-test/utils/RiscZeroSetup.sol
+++ forge-test/utils/RiscZeroSetup.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.13;
 import {RiscZeroGroth16Verifier} from "risc0/groth16/RiscZeroGroth16Verifier.sol";
 
 abstract contract RiscZeroSetup {
-    bytes32 public constant CONTROL_ROOT = hex"a516a057c9fbf5629106300934d48e0e775d4230e41e503347cad96fcbde7e2e";
+    bytes32 public constant CONTROL_ROOT = hex"8b6dcf11d463ac455361b41fb3ed053febb817491bdea00fdb340e45013b852e";
     // NOTE: This has opposite byte order to the value in the risc0 repository.
-    bytes32 public constant BN254_CONTROL_ID = hex"0eb6febcf06c5df079111be116f79bd8c7e85dc9448776ef9a59aaf2624ab551";
+    bytes32 public constant BN254_CONTROL_ID = hex"05a022e1db38457fb510bc347b30eb8f8cf3eda95587653d0eac19e1f10d164e";
 
     RiscZeroGroth16Verifier riscZeroVerifier;
 
