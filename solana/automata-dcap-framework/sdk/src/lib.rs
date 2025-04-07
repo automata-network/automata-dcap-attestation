319b6e03fd299dc3d002cfc3cdb8b7d61257674a
udsamani <uditpsamani@hotmail.com>
2025-04-07 07:35:43 +0100

    feat: add the sdk verify_quote function

--- solana/automata-dcap-framework/sdk/src/lib.rs
+++ solana/automata-dcap-framework/sdk/src/lib.rs
@@ -3,7 +3,41 @@ mod verifier_client;
 mod models;
 mod utils;
 
+use std::ops::Deref;
+
+use anchor_client::solana_sdk::{signature::Signature, signer::Signer};
+use anchor_lang::prelude::Pubkey;
 pub use pccs_client::*;
 pub use verifier_client::*;
 pub use models::*;
 pub use utils::*;
+
+
+/// Verify a quote and return the verified output address and the signatures.
+pub async fn verify_quote<S: Clone + Deref<Target = impl Signer>>(
+    bytes: &[u8],
+    signer:  S
+) -> anyhow::Result<(Pubkey, Vec<Signature>)> {
+
+    let verifier_client = VerifierClient::new(signer)?;
+
+    let quote_buffer_pubkey = verifier_client.init_quote_buffer(
+        bytes.len() as u32,
+        get_num_chunks(bytes.len(), 512),
+    ).await?;
+
+    verifier_client.upload_chunks(
+        quote_buffer_pubkey,
+        bytes,
+        512,
+    ).await?;
+
+    let verified_output_pubkey = verifier_client.init_verified_output_account().await?;
+
+    let signatures = verifier_client.verify_quote(
+        quote_buffer_pubkey,
+        verified_output_pubkey,
+    ).await?;
+
+    Ok((verified_output_pubkey, signatures))
+}
--- solana/automata-dcap-framework/tests/src/verifier/test_quote_verification.rs
+++ solana/automata-dcap-framework/tests/src/verifier/test_quote_verification.rs
@@ -7,27 +7,14 @@ use crate::pccs::get_signer;
 #[tokio::test]
 async fn test_quote_tdx_verification() {
 
-    let client = VerifierClient::new(get_signer()).unwrap();
     let quote_data = include_bytes!("../../data/quote_tdx.bin");
 
-    let quote_buffer_pubkey = client
-        .init_quote_buffer(
-            quote_data.len() as u32,
-            sdk::get_num_chunks(quote_data.len(), 512),
-        )
-        .await.unwrap();
-
-    let verified_output_pubkey = client.init_verified_output_account().await.unwrap();
-
-    client
-        .upload_chunks(quote_buffer_pubkey, quote_data, 512)
-        .await.unwrap();
-
-    let signatures = client.verify_quote(
-        quote_buffer_pubkey,
-        verified_output_pubkey,
+    let (verified_output_pubkey, signatures) = sdk::verify_quote(
+        quote_data,
+        get_signer(),
     ).await.unwrap();
 
+    let client = VerifierClient::new(get_signer()).unwrap();
 
     let verified_output = client.get_account::<VerifiedOutput>(verified_output_pubkey).await.unwrap();
 
