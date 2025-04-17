f4332c60e416aad83d9b8ded90a21bd5d4aab25c
Preston Ong <preston4896@me.com>
2025-04-17 18:03:43 +0800

    verify_pck_cert_chain_zk seems to compile

--- solana/automata-dcap-framework/programs/automata-dcap-verifier/src/errors.rs
+++ solana/automata-dcap-framework/programs/automata-dcap-verifier/src/errors.rs
@@ -31,4 +31,16 @@ pub enum DcapVerifierError {
 
     #[msg("Invalid Secp256r1 instruction")]
     InvalidSecp256r1Instruction,
+
+    #[msg("Invalid Solana ZK Program")]
+    InvalidSolanaZkProgram,
+
+    #[msg("Invalid zkVM selector")]
+    InvalidZkvmSelector,
+
+    #[msg("Invalid zkVM program")]
+    InvalidZkvmProgram,
+
+    #[msg("Invalid zk proof")]
+    InvalidZkProof,
 }
