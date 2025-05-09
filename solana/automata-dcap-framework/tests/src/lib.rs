25b4a3382186968903ea483d8495cdfe01fc74e9
Preston Ong <preston4896@me.com>
2025-05-09 10:35:28 +0800

    load risc0 verifier on genesis

--- solana/automata-dcap-framework/tests/src/lib.rs
+++ solana/automata-dcap-framework/tests/src/lib.rs
@@ -19,9 +19,6 @@ pub fn get_signer() -> Arc<Keypair> {
 mod tests {
     use super::*;
     use sdk::Sdk;
-    use solana_zk_tests::zkvm::risc0::deploy_risc0_groth16_verifier;
-    use anchor_client::solana_client::nonblocking::rpc_client::RpcClient;
-    use anchor_client::solana_sdk::commitment_config::CommitmentConfig;
 
     /// NOTE: Currently I can't specify a timestamp for the local validator to start at.
     /// Tests may fail due to expired collaterals
@@ -31,20 +28,6 @@ mod tests {
         println!("=== SETTING UP TEST ENVIRONMENT ===");
         let signer = get_signer();
         let sdk = Sdk::new(signer.clone(), None);
-
-        let rpc_client = RpcClient::new_with_commitment(
-            String::from("http://localhost:8899"),
-            CommitmentConfig::confirmed(),
-        );
-        if rpc_client
-            .get_account(&TEST_RISC0_VERIFIER_PUBKEY)
-            .await
-            .is_err()
-        {
-            deploy_risc0_groth16_verifier(signer.as_ref(), &rpc_client)
-                .await
-                .unwrap();
-        }
         println!("=== SETTING UP TEST ENVIRONMENT... DONE ===");
 
         println!("=== BEGIN UPSERTING INTEL ROOT CA ===");
