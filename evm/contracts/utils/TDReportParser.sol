604e50867354afb62f264e2c746b40c2b045f44c
Preston Ong <preston4896@me.com>
2025-06-19 10:35:44 +0800

    td15 report parser

--- evm/contracts/utils/TDReportParser.sol
+++ evm/contracts/utils/TDReportParser.sol
@@ -34,7 +34,28 @@ library TD10ReportParser {
 }
 
 library TD15ReportParser {
+    using BytesUtils for bytes;
+
     function parse(bytes memory reportBytes) internal pure returns (bool success, TD15ReportBody memory report) {
-        // TODO
+        success = reportBytes.length == TD_REPORT15_LENGTH;
+        if (success) {
+            report.teeTcbSvn = bytes16(reportBytes.substring(0, 16));
+            report.mrSeam = reportBytes.substring(16, 48);
+            report.mrsignerSeam = reportBytes.substring(64, 48);
+            report.seamAttributes = bytes8(reportBytes.substring(112, 8));
+            report.tdAttributes = bytes8(reportBytes.substring(120, 8));
+            report.xFAM = bytes8(reportBytes.substring(128, 8));
+            report.mrTd = reportBytes.substring(136, 48);
+            report.mrConfigId = reportBytes.substring(184, 48);
+            report.mrOwner = reportBytes.substring(232, 48);
+            report.mrOwnerConfig = reportBytes.substring(280, 48);
+            report.rtMr0 = reportBytes.substring(328, 48);
+            report.rtMr1 = reportBytes.substring(376, 48);
+            report.rtMr2 = reportBytes.substring(424, 48);
+            report.rtMr3 = reportBytes.substring(472, 48);
+            report.reportData = reportBytes.substring(520, 64);
+            report.teeTcbSvn2 = bytes16(reportBytes.substring(584, 16));
+            report.mrServiceTd = reportBytes.substring(600, 48);
+        }
     }
 }
