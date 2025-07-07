829b7a59adc738a26f4e6acfaca0dbf1071d17ad
Preston Ong <preston4896@me.com>
2025-07-08 07:30:20 +0800

    sp1 zk verifier builds

--- solana/automata-dcap-framework/programs/dcap_ecdsa_sp1/src/lib.rs
+++ solana/automata-dcap-framework/programs/dcap_ecdsa_sp1/src/lib.rs
@@ -1,18 +1,45 @@
 #![allow(unexpected_cfgs)]
 
 use anchor_lang::prelude::*;
+use sp1_solana::verify_proof;
 
-declare_id!("Fb5vSs8XwQAopUnrVtMGnVciFaGT4J649nuR1i5kTPQH");
+declare_id!("3tXQLx1CPpecJcBzHBLnC1fvRhLEpHqvWqDRPyxrKDCe");
+
+pub const ECDSA_SP1_DCAP_P256_VKEY: &str =
+    "0x006fca6dbfa6ef2ae092f91a03d49fffdcfb133e48f069fb945a5f98300b2995";
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct SP1Groth16Proof {
+    pub proof: Vec<u8>,
+    pub sp1_public_inputs: Vec<u8>,
+}
 
 #[program]
 pub mod dcap_ecdsa_sp1 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn verify_p256_proof(_ctx: Context<VerifyProof>, instruction_data: Vec<u8>) -> Result<()> {
+        // Deserialize the instruction data into SP1Groth16Proof
+        let groth16_proof = SP1Groth16Proof::try_from_slice(&instruction_data)
+            .map_err(|_| ProgramError::InvalidInstructionData)?;
+
+        let vk = sp1_solana::GROTH16_VK_5_0_0_BYTES;
+
+        let verified = verify_proof(
+            &groth16_proof.proof,
+            &groth16_proof.sp1_public_inputs,
+            &ECDSA_SP1_DCAP_P256_VKEY,
+            vk,
+        );
+
+        if verified.is_err() {
+            msg!("Proof verification failed");
+            return Err(ProgramError::InvalidArgument.into());
+        }
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct VerifyProof {}
